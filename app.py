3a44b2a830b8c04a0026af29f49f8f7acc5c8dc5
Mina Husseinpour <92226453+TheBuffMinashi@users.noreply.github.com>
2022-05-08 15:54:02 +0430

    Update app.py

--- app.py
+++ app.py
@@ -6,40 +6,28 @@ from urllib.parse import parse_qs, urlparse
 st.title('A Simple Streamlit Web App')
 url = st.text_input('Enter YouTube playlist URL here:', '')
 
-st.write("check out this [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
-st.write("check out this [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
-st.write("check out this [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
-# if len(url) != 0:
 
-#     query = parse_qs(urlparse(url).query, keep_blank_values=True)
-#     playlist_id = query["list"][0]
-#     youtube = googleapiclient.discovery.build("youtube", "v3", developerKey = "AIzaSyBMbhy2LT47Y_ovvahfYSmsjp8-whtFgco")
+if len(url) != 0:
+  
+  query = parse_qs(urlparse(url).query, keep_blank_values=True)
+  playlist_id = query["list"][0]
+  youtube = googleapiclient.discovery.build("youtube", "v3", developerKey = "AIzaSyBMbhy2LT47Y_ovvahfYSmsjp8-whtFgco")
 
-#     request = youtube.playlistItems().list(
-#         part = "snippet",
-#         playlistId = playlist_id,
-#         maxResults = 50
-#     )
-#     response = request.execute()
+  request = youtube.playlistItems().list(
+      part = "snippet",
+      playlistId = playlist_id,
+      maxResults = 50
+  )
+  response = request.execute()
 
-#     playlist_items = []
-#     while request is not None:
-#         response = request.execute()
-#         playlist_items += response["items"]
-#         request = youtube.playlistItems().list_next(request, response)
-
-#     links = []
-
-#     for t in playlist_items:
-#       link = 'https://www.youtube.com/watch?v=' + t["snippet"]["resourceId"]["videoId"] + '&list=' + t["id"] + '&t=0s'
-#       name = t["snippet"]["title"]
-#       links.append(link)
+  playlist_items = []
+  while request is not None:
+      response = request.execute()
+      playlist_items += response["items"]
+      request = youtube.playlistItems().list_next(request, response)
 
 
-
-#     df = pd.DataFrame (links, columns = ['Individual Video Link'])
-#     df.index += 1
-#     def make_clickable(val):
-#         return '<a href="{}">{}</a>'.format(val,name)
-#     df.style.format(make_clickable)
-#     st.write(df)
+  for t in playlist_items:
+    link = 'https://www.youtube.com/watch?v=' + t["snippet"]["resourceId"]["videoId"] + '&list=' + t["id"] + '&t=0s'
+    name = t["snippet"]["title"]
+    st.write([name](link))
