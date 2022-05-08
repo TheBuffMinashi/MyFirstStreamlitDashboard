aa0ebcb20cc4e6cf5061cfd6a41860c829514ddc
Mina Husseinpour <92226453+TheBuffMinashi@users.noreply.github.com>
2022-05-08 15:13:00 +0430

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,44 @@ import googleapiclient.discovery
 from urllib.parse import parse_qs, urlparse
 
 st.title('A Simple Streamlit Web App')
-name = st.text_input('Enter YouTube playlist URL here:', '')
+url = st.text_input('Enter YouTube playlist URL here:', '')
 
-df = pd.DataFrame({'x': [x], 'y': [y] , 'x + y': [x + y]}, index = ['addition row'])
-st.write(df)
+
+query = parse_qs(urlparse(url).query, keep_blank_values=True)
+playlist_id = query["list"][0]
+youtube = googleapiclient.discovery.build("youtube", "v3", developerKey = "AIzaSyBMbhy2LT47Y_ovvahfYSmsjp8-whtFgco")
+
+request = youtube.playlistItems().list(
+    part = "snippet",
+    playlistId = playlist_id,
+    maxResults = 50
+)
+response = request.execute()
+
+playlist_items = []
+while request is not None:
+    response = request.execute()
+    playlist_items += response["items"]
+    request = youtube.playlistItems().list_next(request, response)
+
+links = []
+
+for t in playlist_items:
+  link = 'https://www.youtube.com/watch?v=' + t["snippet"]["resourceId"]["videoId"] + '&list=' + t["id"] + '&t=0s'
+  name = t["snippet"]["title"]
+  print(name)
+  # print(link)
+  links.append(link)
+
+import pandas as pd
+# print(links)
+df = pd.DataFrame (links, columns = ['Individual Video Link'])
+df.index += 1
+# df
+def make_clickable(val):
+    return '<a href="{}">{}</a>'.format(val,name)
+
+df.style.format(make_clickable)
+
+
+
