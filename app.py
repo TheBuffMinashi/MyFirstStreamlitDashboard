73bce80c0a406016c18f7cf8c70f1574fe35c48e
Mina Husseinpour <92226453+TheBuffMinashi@users.noreply.github.com>
2022-05-08 13:53:54 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
+import googleapiclient.discovery
+from urllib.parse import parse_qs, urlparse
+
 st.title('A Simple Streamlit Web App')
-name = st.text_input('Enter your name', '')
-st.write(f'Hello {name}!')
-x = st.slider('Select an integer x', 0, 10, 1)
-y = st.slider('Select an integer y', 0, 10, 1)
+name = st.text_input('Enter YouTube playlist URL here:', '')
+
 df = pd.DataFrame({'x': [x], 'y': [y] , 'x + y': [x + y]}, index = ['addition row'])
 st.write(df)
